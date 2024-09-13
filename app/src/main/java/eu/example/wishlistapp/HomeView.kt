78bc0c4e53791f8ad3c35d6d6afe69461fbbc3b5
Alizhan <alizhan20052@gmail.com>
2024-09-13 20:11:35 +0500

    App Bar

--- app/src/main/java/eu/example/wishlistapp/HomeView.kt
+++ app/src/main/java/eu/example/wishlistapp/HomeView.kt
@@ -3,17 +3,39 @@ package eu.example.wishlistapp
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 
 fun HomeView(){
     Scaffold(
-        topBar = {AppBarView(title = "WishList")}
-    ) {
-        LazyColumn(modifier = Modifier.fillMaxSize().padding(it)){
+        topBar = {AppBarView(title = "WishList")},
+        floatingActionButton = {
+            FloatingActionButton(
+                modifier = Modifier.padding(20.dp),
+                contentColor = Color.White,
+                backgroundColor = Color.Black,
+                onClick = { // Add Nav
+                     })
+                 {
+                Icon(imageVector = Icons.Default.Add, contentDescription = null)
+            }
+        }
+
+    )
+
+    {
+        LazyColumn(modifier = Modifier
+            .fillMaxSize()
+            .padding(it)){
 
         }
     }
--- app/src/main/java/eu/example/wishlistapp/MainActivity.kt
+++ app/src/main/java/eu/example/wishlistapp/MainActivity.kt
@@ -29,18 +29,10 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    WishListAppTheme {
-        Greeting("Android")
-    }
+    HomeView()
 }
