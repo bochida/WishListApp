927a6d48ff9b80e28a60ad796789a1dcd24baad1
Alizhan <alizhan20052@gmail.com>
2024-09-13 22:16:27 +0500

    Wishitem and data class

---
+++ app/src/main/java/data/Wish.kt
@@ -0,0 +1,7 @@
+package data
+
+data class Wish (
+    val id: Long = 0L,
+    val title: String = "",
+    val description: String = ""
+)
--- app/src/main/java/eu/example/wishlistapp/HomeView.kt
+++ app/src/main/java/eu/example/wishlistapp/HomeView.kt
@@ -1,17 +1,24 @@
 package eu.example.wishlistapp
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Card
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import data.Wish
 
 @Composable
 
@@ -39,4 +46,25 @@ fun HomeView(){
 
         }
     }
+}
+
+@Composable
+fun WishItem(wish: Wish, onClick: () ->Unit){
+    Card(modifier = Modifier
+        .fillMaxWidth()
+        .padding(top = 8.dp, start = 8.dp, end = 8.dp)
+        .clickable {
+            onClick()
+        },
+        elevation = 10.dp,
+        backgroundColor = Color.White
+
+    ){
+        Column(modifier = Modifier.padding(16.dp)){
+            Text(text = wish.title, fontWeight = FontWeight.ExtraBold)
+            Text(text = wish.description)
+        }
+    }
+
+
 }
